6e2478783e640d7bf165f2755b1feb11c5de3a72
beni <dayanbeni@gmail.com>
2020-04-04 20:56:01 +0300

    commit toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-//
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// //
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import 'hammerjs'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, BrowserAnimationsModule, MatToolbarModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
