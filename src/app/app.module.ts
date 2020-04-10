190e374b09441545fe9d0e6902209163b1be1172
beni <dayanbeni@gmail.com>
2020-04-10 15:03:00 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import{UpperCasePipe} from '@angular/common'
-import { MatListModule } from '@angular/material/list';
+//import{UpperCasePipe} from '@angular/common'
+//import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { BrowserModule } from '@angular/platform-browser';
+//import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent, appmodule } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import 'hammerjs';
@@ -14,8 +14,10 @@ import { Dish } from './shared/dish';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 @NgModule({
   declarations: [AppComponent, MenuComponent, DishdetailComponent],
-  imports: [  MatListModule,MatToolbarModule,
-    MatCardModule, BrowserAnimationsModule,
+  imports: [  MatListModule,
+    MatToolbarModule,
+    MatCardModule,
+     BrowserAnimationsModule,
     , FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent]})
